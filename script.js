da480c5bf2187e1306462dc273e056111ca7c20d
GusiGusi08 <gustavki@ofk.no>
2025-03-04 17:25:48 +0100

    hsdadads
    asdasdasdsada

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 window.addEventListener("resize", function() {
-    if (window.innerWidth < 589) {
-        document.body.style.zoom = window.innerWidth / 590 + "";
+    if (window.innerWidth < 601) {
+        document.body.style.zoom = window.innerWidth / 600 + "";
     } else {
         document.body.style.zoom = "";
     }
-});
+  })
+  window.addEventListener("load", function() {
+    if (window.innerWidth < 600) {
+        document.body.style.zoom = window.innerWidth / 599 + "";
+    } else {
+        document.body.style.zoom = "";
+    }
+  })
+
