8e4c077a6a5d6c6d7b921f866cc5c409b3e9e824
GusiGusi08 <gustavki@ofk.no>
2025-03-04 08:58:57 +0100

    jeg er
    jeg eer ikke hjer

---
+++ script.js
@@ -0,0 +1,6 @@
+function greetUser(name) {
+    alert(`Hei, ${name}! Velkommen til vår nettside. Du har nå lagra siden`);
+}
+
+greetUser(prompt("Skriv et navn får å lagre siden til neste gang du er her:)"));
+
